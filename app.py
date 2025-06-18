fce322a21adb3d5ae9cb2271855fb182621533b2
samylesousa <samylesousa@alu.ufc.br>
2025-06-17 22:36:40 -0300

    atualizacoes para os testes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from schema import schema
+import os
 
 # Criando a instância do FastAPI
 app = FastAPI()
@@ -8,4 +9,5 @@ app = FastAPI()
 # Adicionando a rota GraphQL
 graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
+print(f"API GraphQL rodando com PID: {os.getpid()}")
 
--- database_config.py
+++ database_config.py
@@ -1,9 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-DATABASE_URL = "mysql+asyncmy://root:issoechatopracaralho@localhost:3306/dadosGerais"
+DATABASE_URL = os.getenv("DATABASE")
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
